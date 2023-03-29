7762c6fc7d0508e31459e4a1860c37c7c1e97fd4
MatthewJMoldovan <120879285+MatthewJMoldovan@users.noreply.github.com>
2023-03-29 14:56:11 -0600

    Feature/view one, fixes to UserForm (#5)
    * created UsersList component, DeleteButton component, and ViewAll view
    
    * created feature/ViewOne. Fixes to UserForm

--- apps/client/src/views/ViewOne.js
+++ apps/client/src/views/ViewOne.js
@@ -1,7 +1,41 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { UserForm } from '../components/UserForm';
 
 export const OneUser = (props) => {
   const { id } = useParams();
 
-  return <h2>One User</h2>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/users/${id}`)
+      .then((res) => {
+        setUser(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  if (user === null) {
+    return <h1>Loading...Pleast Wait!</h1>;
+  }
+
+  // name: '',
+  // age: '',
+  // gender: '',
+  // height: '',
+  // weight: '',
+  // activityLevel: '',
+  // goal: '',
+  const { name, age, height, weight, activityLevel, goal } = user;
+
+  return (
+    <div className="container">
+      <UserForm formType={'update'} user={user} setUser={setUser} />
+    </div>
+  );
 };
